53042b8036fac800226a604208589c495b8e2e5b
misiekhardcore <misiekhardcore@gmail.com>
2021-04-14 14:27:05 +0200

    add mongodb configs

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -2,6 +2,30 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const MONGO_OPTIONS = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  socketTimeoutMS: 30000,
+  keepAlive: true,
+  poolSize: 50,
+  autoIndex: false,
+  retryWrites: false,
+};
+
+const MONGO_USERNAME = process.env.MONGO_USERNAME || "user";
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "password";
+const MONGO_HOST = process.env.MONGO_URL || "";
+const MONGO_DB = process.env.MONGO_DB || "";
+
+const MONGO = {
+  host: MONGO_HOST,
+  username: MONGO_USERNAME,
+  password: MONGO_PASSWORD,
+  options: MONGO_OPTIONS,
+  db: MONGO_DB,
+  url: `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DB}`,
+};
+
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || "localhost";
 const SERVER_PORT = process.env.PORT;
 
@@ -11,6 +35,7 @@ const SERVER = {
 };
 
 const config = {
+  mongo: MONGO,
   server: SERVER,
 };
 
