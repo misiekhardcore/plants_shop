b9a95152a2898bec87f42b75c807ebf9787623c8
misiekhardcore <misiekhardcore@gmail.com>
2021-04-14 21:02:38 +0200

    add jwt vars

--- backend/src/env.d.ts
+++ backend/src/env.d.ts
@@ -1,9 +1,12 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    PORT: string;
+    SERVER_PORT: string;
     MONGO_USERNAME: string;
     MONGO_PASSWORD: string;
     MONGO_URL: string;
     MONGO_DB: string;
+    SERVER_TOKEN_EXPIRETIME: string;
+    SERVER_TOKEN_ISSUER: string;
+    SERVER_TOKEN_SECRET: string;
   }
 }
