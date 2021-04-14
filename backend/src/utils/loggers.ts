0e792a48f59651f081afaa78cfe484b38a8d2171
misiekhardcore <misiekhardcore@gmail.com>
2021-04-14 14:28:47 +0200

    remove object arg from loggers

--- backend/src/utils/loggers.ts
+++ backend/src/utils/loggers.ts
@@ -3,28 +3,24 @@ const getTimeStamp = (): string => {
 };
 
 const loggings = {
-  info: (namespace: string, message: string, object: any = "") => {
+  info: (namespace: string, message: string) => {
     console.info(
-      `\x1b[2m[${getTimeStamp()}]\x1b[0m \x1b[32m[INFO]\x1b[0m \x1b[2m[${namespace}]\x1b[0m ${message}`,
-      object
+      `\x1b[2m[${getTimeStamp()}]\x1b[0m \x1b[32m[INFO]\x1b[0m \x1b[2m[${namespace}]\x1b[0m ${message}`
     );
   },
-  warn: (namespace: string, message: string, object: any = "") => {
+  warn: (namespace: string, message: string) => {
     console.warn(
-      `\x1b[2m[${getTimeStamp()}]\x1b[0m \x1b[33m[WARN]\x1b[0m \x1b[2m[${namespace}]\x1b[0m ${message}`,
-      object
+      `\x1b[2m[${getTimeStamp()}]\x1b[0m \x1b[33m[WARN]\x1b[0m \x1b[2m[${namespace}]\x1b[0m ${message}`
     );
   },
-  error: (namespace: string, message: string, object: any = "") => {
+  error: (namespace: string, message: string) => {
     console.error(
-      `\x1b[2m[${getTimeStamp()}]\x1b[0m \x1b[31m[ERROR]\x1b[0m \x1b[2m[${namespace}]\x1b[0m ${message}`,
-      object
+      `\x1b[2m[${getTimeStamp()}]\x1b[0m \x1b[31m[ERROR]\x1b[0m \x1b[2m[${namespace}]\x1b[0m ${message}`
     );
   },
-  debug: (namespace: string, message: string, object: any = "") => {
+  debug: (namespace: string, message: string) => {
     console.debug(
-      `\x1b[2m[${getTimeStamp()}]\x1b[0m \x1b[35m[DEBUG]\x1b[0m \x1b[2m[${namespace}]\x1b[0m ${message}`,
-      object
+      `\x1b[2m[${getTimeStamp()}]\x1b[0m \x1b[35m[DEBUG]\x1b[0m \x1b[2m[${namespace}]\x1b[0m ${message}`
     );
   },
 };
