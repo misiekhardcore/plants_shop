5086b959e0e36b092ea96fccd128c14bd50a5a7b
misiekhardcore <misiekhardcore@gmail.com>
2021-04-16 23:47:00 +0200

    add typing and clg

--- backend/src/controllers/products.ts
+++ backend/src/controllers/products.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { CustomReqBody } from "src/interfaces/common";
+import { CustomReqBody, Error } from "src/interfaces/common";
 import { IProduct, IProductUpdate } from "../interfaces/product";
 import { Product } from "../models/product.model";
 import loggings from "../utils/loggers";
@@ -8,7 +8,7 @@ const NAMESPACE = "products";
 
 export const getAllProducts = async (
   _: Request,
-  res: Response
+  res: Response<IProduct[] | Error>
 ): Promise<void> => {
   try {
     const products = await Product.find({});
@@ -22,7 +22,7 @@ export const getAllProducts = async (
 
 export const getOneProduct = async (
   req: Request<{ id: string }>,
-  res: Response,
+  res: Response<IProduct | Error>,
   next: NextFunction
 ): Promise<void> => {
   try {
@@ -33,18 +33,19 @@ export const getOneProduct = async (
     res.status(200).json(product);
   } catch (error) {
     loggings.error(NAMESPACE, error.message);
+    res.status(500).json({ message: "server error", error });
   }
 };
 
 export const createProduct = async (
   req: CustomReqBody<IProduct>,
-  res: Response,
+  res: Response<IProduct | Error>,
   next: NextFunction
 ): Promise<void> => {
   try {
     const product = await Product.create<IProduct>(req.body);
     if (!product) return next();
-
+    loggings.info(NAMESPACE, "product created");
     res.status(201).json(product);
   } catch (error) {
     if (error.errors.description.kind === "required") {
@@ -59,20 +60,29 @@ export const createProduct = async (
 
 export const updateProduct = async (
   req: CustomReqBody<IProductUpdate>,
-  res: Response<IProduct>,
+  res: Response<IProduct | Error>,
   next: NextFunction
 ): Promise<void> => {
   try {
     await Product.updateOne({ _id: req.body._id }, req.body);
     const product = await Product.findById(req.body._id);
     if (!product) return next();
+    loggings.info(NAMESPACE, "product updated");
     res.status(200).json(product);
-  } catch (error) {}
+  } catch (error) {
+    if (error.errors.description.kind === "required") {
+      loggings.warn(NAMESPACE, "user input error");
+      res.status(400).json({ message: "user input error", error });
+      return;
+    }
+    loggings.error(NAMESPACE, error.message);
+    res.status(500).json({ message: "server error", error });
+  }
 };
 
 export const deleteProduct = async (
   req: Request<{ id: string }>,
-  res: Response,
+  res: Response<Error>,
   next: NextFunction
 ): Promise<void> => {
   const { id } = req.params;
@@ -81,6 +91,7 @@ export const deleteProduct = async (
 
   try {
     await Product.deleteOne({ _id: id });
+    loggings.info(NAMESPACE, "product deleted");
     res.status(200).json({ message: "deleted" });
   } catch (error) {
     res.status(500).json(error);
