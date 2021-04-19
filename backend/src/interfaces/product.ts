0c632eaaff6e6ca6d27fd49564e4cc667b23645b
misiekhardcore <misiekhardcore@gmail.com>
2021-04-19 19:14:30 +0200

    start adding fields to product interface

--- backend/src/interfaces/product.ts
+++ backend/src/interfaces/product.ts
@@ -1,11 +1,21 @@
 import { Document, Model } from "mongoose";
+import { IComment } from "./comment";
+
+export type TLevels = "low" | "medium" | "high";
 
 export interface IProduct {
   name: string;
   description: string;
+  longDescription: string;
   price: number;
   countInStock: number;
   imgURLs: string[];
+  discount: number;
+  rating: number;
+  comments: IComment[];
+  light: TLevels;
+  watering: TLevels;
+  temperature: number;
 }
 
 export interface IProductUpdate extends IProduct {
