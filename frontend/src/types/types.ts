a0d4a617270741cff3b19e7c901fb8337ec36afb
misiekhardcore <misiekhardcore@gmail.com>
2021-04-19 19:17:40 +0200

    add sortBy types

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -20,3 +20,13 @@ export interface Error {
 }
 
 export type ActionStatus = "idle" | "loading" | "failed";
+
+export const SortBy = {
+  NONE: {},
+  PA: { price: "asc" },
+  PD: { price: "desc" },
+  NA: { name: "asc" },
+  ND: { name: "desc" },
+};
+
+export type sortByEnum = keyof typeof SortBy;
