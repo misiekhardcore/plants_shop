9f07d6b55d29fcb1423d170a7a403b70745e4100
misiekhardcore <misiekhardcore@gmail.com>
2021-04-19 12:50:10 +0200

    change loading state and error type

--- frontend/src/redux/slices/productsSlice.ts
+++ frontend/src/redux/slices/productsSlice.ts
@@ -1,18 +1,17 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
 import {
-  ActionStatus,
-  Error,
-  IProduct,
-  IProductInput,
-} from "../../types/types";
+  createAsyncThunk,
+  createSlice,
+  SerializedError,
+} from "@reduxjs/toolkit";
+import axios from "axios";
+import { IProduct, IProductInput } from "../../types/types";
 import { RootState } from "../store";
 
 export interface ProductsState {
   products: IProduct[];
   productDetails: IProduct;
-  status: ActionStatus;
-  error: any;
+  loading: boolean;
+  error: SerializedError | undefined;
 }
 
 const initialState: ProductsState = {
@@ -25,14 +24,14 @@ const initialState: ProductsState = {
     name: "",
     price: 0,
   },
-  status: "idle",
-  error: {},
+  loading: false,
+  error: undefined,
 };
 
 export const getAllProducts = createAsyncThunk<
   IProduct[],
   void,
-  { rejectValue: Error }
+  { rejectValue: SerializedError }
 >("products/getAllProducts", async (_, { rejectWithValue }) => {
   try {
     const response = await axios.get<IProduct[]>(
@@ -47,69 +46,60 @@ export const getAllProducts = createAsyncThunk<
 export const getOneProduct = createAsyncThunk<
   IProduct,
   { id: string },
-  { state: RootState; rejectValue: Error }
->(
-  "products/getOneProduct",
-  async ({ id }, { getState, rejectWithValue }) => {
-    const {
-      products: { productDetails },
-    } = getState();
-    try {
-      if (productDetails._id !== id) {
-        const response = await axios.get<IProduct>(
-          `http://localhost:4000/api/products/${id}`
-        );
-        return response.data;
-      }
-      return productDetails;
-    } catch (error) {
-      return rejectWithValue(error.response.data);
+  { state: RootState; rejectValue: SerializedError }
+>("products/getOneProduct", async ({ id }, { getState, rejectWithValue }) => {
+  const {
+    products: { productDetails },
+  } = getState();
+  try {
+    if (productDetails._id !== id) {
+      const response = await axios.get<IProduct>(
+        `http://localhost:4000/api/products/${id}`
+      );
+      return response.data;
     }
+    return productDetails;
+  } catch (error) {
+    return rejectWithValue(error.response.data);
   }
-);
+});
 
 export const createProduct = createAsyncThunk<
   IProduct,
   { product: IProductInput },
-  { rejectValue: Error }
->(
-  "products/createProduct",
-  async ({ product }, { rejectWithValue }) => {
-    try {
-      const response = await axios.post<IProduct>(
-        "http://localhost:4000/api/products",
-        product
-      );
-      return response.data;
-    } catch (error) {
-      return rejectWithValue(error.response.data);
-    }
+  { rejectValue: SerializedError }
+>("products/createProduct", async ({ product }, { rejectWithValue }) => {
+  try {
+    const response = await axios.post<IProduct>(
+      "http://localhost:4000/api/products",
+      product
+    );
+    return response.data;
+  } catch (error) {
+    return rejectWithValue(error.response.data);
   }
-);
+});
 
 export const updateProduct = createAsyncThunk<
   IProduct,
   { product: IProduct },
-  { rejectValue: Error }
->(
-  "products/updateProduct",
-  async ({ product }, { rejectWithValue }) => {
-    try {
-      const response = await axios.put<IProduct>(
-        "http://localhost:4000/api/products",
-        product
-      );
-      return response.data;
-    } catch (error) {
-      return rejectWithValue(error.response.data);
-    }
+  { rejectValue: SerializedError }
+>("products/updateProduct", async ({ product }, { rejectWithValue }) => {
+  try {
+    const response = await axios.put<IProduct>(
+      "http://localhost:4000/api/products",
+      product
+    );
+    return response.data;
+  } catch (error) {
+    return rejectWithValue(error.response.data);
   }
-);
+});
 
 export const deleteProduct = createAsyncThunk<
   boolean,
   { id: string },
-  { rejectValue: Error }
+  { rejectValue: SerializedError }
 >("products/deleteProduct", async ({ id }, { rejectWithValue }) => {
   try {
     const response = await axios.delete<boolean>(
@@ -128,66 +118,67 @@ export const productsSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(getAllProducts.pending, (state) => {
-        state.status = "loading";
+        state.loading = true;
       })
       .addCase(getAllProducts.fulfilled, (state, action) => {
-        state.status = "idle";
+        state.loading = false;
         state.products = action.payload;
-        state.error = {};
+        state.error = undefined;
       })
       .addCase(getAllProducts.rejected, (state, action) => {
-        state.status = "failed";
+        state.loading = false;
         state.error = action.payload;
       })
       .addCase(getOneProduct.pending, (state) => {
-        state.status = "loading";
+        state.loading = true;
       })
       .addCase(getOneProduct.fulfilled, (state, action) => {
-        state.status = "idle";
+        state.loading = false;
         state.productDetails = action.payload;
-        state.error = {};
+        state.error = undefined;
       })
       .addCase(getOneProduct.rejected, (state, action) => {
-        state.status = "failed";
+        state.loading = false;
         state.error = action.payload;
       })
       .addCase(createProduct.pending, (state) => {
-        state.status = "loading";
+        state.loading = true;
       })
       .addCase(createProduct.fulfilled, (state, action) => {
-        state.status = "idle";
+        state.loading = false;
         state.products.push(action.payload);
-        state.error = {};
+        state.error = undefined;
       })
       .addCase(createProduct.rejected, (state, action) => {
-        state.status = "failed";
+        state.loading = false;
         state.error = action.payload;
       })
       .addCase(updateProduct.pending, (state) => {
-        state.status = "loading";
+        state.loading = true;
       })
       .addCase(updateProduct.fulfilled, (state, action) => {
-        state.status = "idle";
+        state.loading = false;
         state.products = state.products.map((p) => {
           if (p._id === action.payload._id) {
             return action.payload;
           }
           return p;
         });
-        state.error = {};
+        state.error = undefined;
       })
       .addCase(updateProduct.rejected, (state, action) => {
-        state.status = "failed";
+        state.loading = false;
         state.error = action.payload;
       })
       .addCase(deleteProduct.pending, (state) => {
-        state.status = "loading";
+        state.loading = true;
       })
       .addCase(deleteProduct.fulfilled, (state, action) => {
-        state.status = "idle";
+        state.loading = false;
         state.products = state.products.filter(
           (p) => p._id !== action.meta.arg.id
         );
+        state.error = undefined;
       });
   },
 });
