9c19fd4f5de5975975d3e03efb1571697b93f912
misiekhardcore <misiekhardcore@gmail.com>
2021-04-19 12:49:16 +0200

    add temporary styling for testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Product } from "./components/Product";
+import { useAppDispatch, useAppSelector } from "./hooks/hooks";
+import { getAllProducts, selectProducts } from "./redux/slices/productsSlice";
+
+import "./App.scss";
+import { selectCart } from "./redux/slices/cartSlice";
+import getTotalAmount from "./redux/utils/getTotalAmount";
+import getTotalPrice from "./redux/utils/getTotalPrice";
 
 interface AppProps {}
 
 const App: React.FC<AppProps> = () => {
-  return <div></div>;
+  const dispatch = useAppDispatch();
+  const { error, loading, products } = useAppSelector(selectProducts);
+  const cart = useAppSelector(selectCart);
+  useEffect(() => {
+    dispatch(getAllProducts());
+  }, [dispatch]);
+  return (
+    <div className="container">
+      {loading && <p>Loading...</p>}
+      {products && (
+        <div className="products">
+          {products.map((p) => (
+            <Product key={p._id} product={p} />
+          ))}
+        </div>
+      )}
+      {error && <p>{error.message}</p>}
+      <div className="">
+        {cart.map((c) => (
+          <p>{`${c._id} ${c.amount}`}</p>
+        ))}
+      </div>
+      <p>{getTotalAmount(cart)}</p>
+      <p>${getTotalPrice(cart)}</p>
+    </div>
+  );
 };
 
 export default App;
