a2817c0e74637651ee5702f915ec499905f84abb
misiekhardcore <misiekhardcore@gmail.com>
2021-04-19 19:15:22 +0200

    add getAllProducts params

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 import { Product } from "./components/Product";
 import { useAppDispatch, useAppSelector } from "./hooks/hooks";
-import { getAllProducts, selectProducts } from "./redux/slices/productsSlice";
+import {
+  getAllProducts,
+  selectProducts,
+} from "./redux/slices/productsSlice";
 
 import "./App.scss";
 import { selectCart } from "./redux/slices/cartSlice";
@@ -14,7 +17,7 @@ const App: React.FC<AppProps> = () => {
   const { error, loading, products } = useAppSelector(selectProducts);
   const cart = useAppSelector(selectCart);
   useEffect(() => {
-    dispatch(getAllProducts({limit:5}));
+    dispatch(getAllProducts({ limit: 100, offset: 0, sortBy:'NA' }));
   }, [dispatch]);
   return (
     <div className="container">
@@ -32,7 +35,7 @@ const App: React.FC<AppProps> = () => {
       {!cart.length && <p>Cart is empty</p>}
       <ul className="cartproducts">
         {cart.map((c) => (
-          <CartProduct product={c} />
+          <CartProduct key={c._id} product={c} />
         ))}
       </ul>
     </div>
