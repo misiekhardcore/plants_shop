d229c593094fc20e351d2bca096574ea5b8fdd78
misiekhardcore <misiekhardcore@gmail.com>
2021-04-20 13:12:23 +0200

    move from App to separate components and screens

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,35 @@
-import React, { useEffect } from "react";
-import { Product } from "./components/Product";
-import { useAppDispatch, useAppSelector } from "./hooks/hooks";
-import {
-  getAllProducts,
-  selectProducts,
-} from "./redux/slices/productsSlice";
-
+import React from "react";
 import "./App.scss";
-import { selectCart } from "./redux/slices/cartSlice";
 import { CartProduct } from "./components/CartProduct";
+import { Navbar } from "./components/Navbar";
+import { useAppSelector } from "./hooks/hooks";
+import { selectCart } from "./redux/slices/cartSlice";
+import { CreateProduct } from "./screens/CreateProduct";
+import { Hero } from "./screens/Hero";
+import { ProductsSection } from "./screens/ProductsSection";
 
 interface AppProps {}
 
 const App: React.FC<AppProps> = () => {
-  const dispatch = useAppDispatch();
-  const { error, loading, products } = useAppSelector(selectProducts);
   const cart = useAppSelector(selectCart);
-  useEffect(() => {
-    dispatch(getAllProducts({ limit: 100, offset: 0, sortBy:'NA' }));
-  }, [dispatch]);
+
   return (
-    <div className="container">
-      <h2>Products</h2>
-      {loading && <p>Loading...</p>}
-      {products && (
-        <div className="products">
-          {products.map((p) => (
-            <Product key={p._id} product={p} />
+    <>
+      <Navbar />
+      <Hero />
+      <ProductsSection title="Sale" />
+      <ProductsSection title="Bestsellers" />
+      <div className="container">
+        <h2>Cart</h2>
+        {!cart.length && <p>Cart is empty</p>}
+        <ul className="cartproducts">
+          {cart.map((c) => (
+            <CartProduct key={c._id} product={c} />
           ))}
-        </div>
-      )}
-      {error && <p>{error.message}</p>}
-      <h2>Cart</h2>
-      {!cart.length && <p>Cart is empty</p>}
-      <ul className="cartproducts">
-        {cart.map((c) => (
-          <CartProduct key={c._id} product={c} />
-        ))}
-      </ul>
-    </div>
+        </ul>
+      </div>
+      <CreateProduct />
+    </>
   );
 };
 
