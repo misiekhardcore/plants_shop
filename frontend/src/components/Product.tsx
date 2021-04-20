cb486ff4e5e1d9c34d69b100e1401045f2924690
misiekhardcore <misiekhardcore@gmail.com>
2021-04-20 21:35:36 +0200

    add discount indicator

--- frontend/src/components/Product.tsx
+++ frontend/src/components/Product.tsx
@@ -12,7 +12,15 @@ interface ProductProps {
 
 export const Product: React.FC<ProductProps> = ({ product }) => {
   const dispatch = useAppDispatch();
-  const { _id, name, imgURLs, price, description } = product;
+  const {
+    _id,
+    name,
+    imgURLs,
+    price,
+    description,
+    discount = 0,
+    rating = 0,
+  } = product;
   return (
     <div className="product__card">
       <Link to={`/products/${_id}`}>
@@ -24,18 +32,27 @@ export const Product: React.FC<ProductProps> = ({ product }) => {
             })`,
           }}
         >
-          <div className="dummy"></div>
+          <div className="dummy">
+            {rating > 0 && <div className="rating"></div>}
+            {discount > 0 && (
+              <div className="product__discount">-{discount}%</div>
+            )}
+          </div>
         </div>
       </Link>
       <div className="product__info">
         <h2 className="product__title" title={name}>
-          {name}
+          <Link to={`/products/${_id}`}>{name}</Link>
         </h2>
-        <p className="product__description">{description.slice(0, 50)}</p>
+        <p className="product__description">
+          {description.slice(0, 50)}
+        </p>
         <div className="product__price">
           <p>${price}</p>
           <button
-            onClick={() => dispatch(addToCart({ ...product, amount: 1 }))}
+            onClick={() =>
+              dispatch(addToCart({ ...product, amount: 1 }))
+            }
           >
             Add to cart
           </button>
