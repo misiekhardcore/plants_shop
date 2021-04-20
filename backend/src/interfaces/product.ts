a6aad460636aa93f512b635d4196aabe0eab1ba5
misiekhardcore <misiekhardcore@gmail.com>
2021-04-20 13:08:33 +0200

    add fields to Product

--- backend/src/interfaces/product.ts
+++ backend/src/interfaces/product.ts
@@ -16,6 +16,7 @@ export interface IProduct {
   light: TLevels;
   watering: TLevels;
   temperature: number;
+  saled: number;
 }
 
 export interface IProductUpdate extends IProduct {
--- backend/src/models/product.schema.ts
+++ backend/src/models/product.schema.ts
@@ -5,9 +5,21 @@ export const ProductSchema = new Schema<IProductDocument>(
   {
     name: { type: String, required: true },
     description: { type: String, required: true },
-    imgURLs: [{ type: String, required: true }],
+    longDescription: { type: String, required: true },
     price: { type: Number, required: true },
     countInStock: { type: Number, required: true },
+    imgURLs: [{ type: String, required: true }],
+    discount: { type: Number, default: 0 },
+    rating: { type: Number, default: 5 },
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+    light: { type: String, enum: ["low", "medium", "high"], default: "medium" },
+    watering: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "medium",
+    },
+    temperature: { type: Number, default: 20 },
+    saled: { type: Number, default: 0 },
   },
   {
     timestamps: true,
