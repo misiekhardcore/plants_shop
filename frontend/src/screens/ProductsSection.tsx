1ba4fba747d0cfb4b124b57ea3db3bac8fff1d89
misiekhardcore <misiekhardcore@gmail.com>
2021-04-20 21:39:05 +0200

    use getAllProducts request type

--- frontend/src/screens/ProductsSection.tsx
+++ frontend/src/screens/ProductsSection.tsx
@@ -1,25 +1,31 @@
 import React, { useEffect } from "react";
 import { Product } from "../components/Product";
 import { useAppDispatch, useAppSelector } from "../hooks/hooks";
-import { getAllProducts, selectProducts } from "../redux/slices/productsSlice";
+import {
+  getAllProducts,
+  selectProducts,
+} from "../redux/slices/productsSlice";
+import { IGetProductsReq } from "../types/types";
 
 import "./ProductsSection.scss";
 
-interface ProductsSectionProps {
-  limit?: number;
-  title?: string;
+interface ProductsSectionProps extends IGetProductsReq {
+  title: string;
 }
 
 export const ProductsSection: React.FC<ProductsSectionProps> = ({
   limit = 4,
+  offset = 0,
   title = "Products",
+  search = {},
+  sortBy = "NONE",
 }) => {
   const dispatch = useAppDispatch();
   const { error, loading, products } = useAppSelector(selectProducts);
 
   useEffect(() => {
-    dispatch(getAllProducts({ limit }));
-  }, [dispatch, limit]);
+    dispatch(getAllProducts({ limit, offset, search, sortBy }));
+  }, [dispatch, limit, offset, search, sortBy]);
   return (
     <div className="container">
       <div className="row">
