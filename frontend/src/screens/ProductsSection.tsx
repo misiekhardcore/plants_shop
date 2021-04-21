54874ae49c47028d3a036420ee35d2fead4143bc
misiekhardcore <misiekhardcore@gmail.com>
2021-04-21 14:28:35 +0200

    add slider

--- frontend/src/screens/ProductsSection.tsx
+++ frontend/src/screens/ProductsSection.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect } from "react";
 import { Product } from "../components/Product";
+import { SliderProducts } from "../components/Slider";
 import { useAppDispatch, useAppSelector } from "../hooks/hooks";
-import {
-  getAllProducts,
-  selectProducts,
-} from "../redux/slices/productsSlice";
+import { getAllProducts, selectProducts } from "../redux/slices/productsSlice";
 import { IGetProductsReq } from "../types/types";
 
 import "./ProductsSection.scss";
@@ -14,29 +12,29 @@ interface ProductsSectionProps extends IGetProductsReq {
 }
 
 export const ProductsSection: React.FC<ProductsSectionProps> = ({
-  limit = 4,
+  limit = 12,
   offset = 0,
   title = "Products",
   search = {},
-  sortBy = "NONE",
+  sortBy = 'ND',
 }) => {
   const dispatch = useAppDispatch();
   const { error, loading, products } = useAppSelector(selectProducts);
 
   useEffect(() => {
     dispatch(getAllProducts({ limit, offset, search, sortBy }));
-  }, [dispatch, limit, offset, search, sortBy]);
+  }, [dispatch, limit, offset, sortBy]);
   return (
     <div className="container">
       <div className="row">
         <h2>{title}</h2>
         {loading && <p>Loading...</p>}
         {products && (
-          <div className="products">
+          <SliderProducts>
             {products.map((p) => (
               <Product key={p._id} product={p} />
             ))}
-          </div>
+          </SliderProducts>
         )}
         {error && <p>{error.message}</p>}
       </div>
