02e11ed4bd64249d8cec73f3d682ae50cb1bb0c9
misiekhardcore <misiekhardcore@gmail.com>
2021-04-21 20:32:37 +0200

    create styled button

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+interface ButtonProps {
+  size?: "small" | "normal" | "big";
+}
+
+export const Button = styled.button<ButtonProps>`
+  color: ${(props) => props.theme.colors.white};
+  background-color: ${(props) => props.theme.colors.primary};
+  border-radius: ${(props) => props.theme.radius.small};
+  border: 1px solid ${(props) => props.theme.colors.primary};
+  padding: ${({ size = "normal" }) => {
+    switch (size) {
+      case "normal":
+        return ".5rem 1rem";
+      case "small":
+        return ".2rem .4rem";
+      case "big":
+        return "0.75rem 1.5rem";
+      default:
+        return ".5rem 1rem";
+    }
+  }};
+  font-size: ${({ size = "normal" }) => {
+    switch (size) {
+      case "normal":
+        return "1rem";
+      case "small":
+        return ".8rem";
+      case "big":
+        return "1.2rem";
+      default:
+        return "1rem";
+    }
+  }};
+
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  &:hover,
+  &:focus {
+    background-color: ${(props) => props.theme.colors.primaryDark};
+    border-color: ${(props) => props.theme.colors.primaryDark};
+    transform: translateY(-1px);
+  }
+`;
