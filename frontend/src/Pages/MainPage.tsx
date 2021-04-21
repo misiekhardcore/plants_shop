60f3a0757d517397f2924b0a9753702feeb0fcef
misiekhardcore <misiekhardcore@gmail.com>
2021-04-21 14:28:03 +0200

    refactor

--- frontend/src/Pages/MainPage.tsx
+++ frontend/src/Pages/MainPage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { DiscountSection } from "../screens/DiscountSection";
 import { Hero } from "../screens/Hero";
 import { ProductsSection } from "../screens/ProductsSection";
 
@@ -6,12 +7,8 @@ const MainPage: React.FC = () => {
   return (
     <>
       <Hero />
-      <ProductsSection
-        title="Sale"
-        offset={4}
-        search={{ discount: { $gt: 0 } }}
-      />
-      {/* <ProductsSection title="Bestsellers" /> */}
+      <DiscountSection title="Sale" />
+      <ProductsSection title="Bestsellers" />
     </>
   );
 };
