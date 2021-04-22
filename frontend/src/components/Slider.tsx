5da599f7f1ca767ed6f51ea65824c0096f8ace7c
misiekhardcore <misiekhardcore@gmail.com>
2021-04-22 14:18:35 +0200

    add custom arrows

--- frontend/src/components/Slider.tsx
+++ frontend/src/components/Slider.tsx
@@ -1,11 +1,64 @@
 import React from "react";
-import Slider from "react-slick";
+import Slider, { CustomArrowProps } from "react-slick";
+import styled from "styled-components";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+
+const Arrow = styled.div<{ side: "left" | "right" }>`
+  border-radius: 50%;
+  background-color: ${(props) => props.theme.colors.primary};
+  color: white;
+  font-size: 2rem;
+  width: 2rem;
+  height: 2rem;
+  position: absolute;
+  top: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+  ${({ side }) => {
+    switch (side) {
+      case "left":
+        return "left:0;";
+      case "right":
+        return "right:0;";
+      default:
+        return "left:0;";
+    }
+  }}
+
+  &:hover {
+    cursor: pointer;
+    background-color: ${(props) => props.theme.colors.primaryDark};
+  }
+`;
+
+const NextArrow = (props: CustomArrowProps) => {
+  const { onClick } = props;
+
+  return (
+    <Arrow side="right" onClick={onClick}>
+      <BsChevronRight />
+    </Arrow>
+  );
+};
+const PrevArrow = (props: CustomArrowProps) => {
+  const { onClick } = props;
+
+  return (
+    <Arrow side="left" onClick={onClick}>
+      <BsChevronLeft />
+    </Arrow>
+  );
+};
 
 const settings = {
   slidesToShow: 4,
   slidesToScroll: 4,
   speed: 500,
   infinite: false,
+  nextArrow: <NextArrow />,
+  prevArrow: <PrevArrow />,
   responsive: [
     {
       breakpoint: 900,
