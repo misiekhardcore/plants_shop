e3f40459628c14c907d9ef0573634c4a52f9c5a9
misiekhardcore <misiekhardcore@gmail.com>
2021-05-20 14:39:12 +0200

    add basic styling

--- frontend/src/Pages/ProductPage.tsx
+++ frontend/src/Pages/ProductPage.tsx
@@ -3,20 +3,12 @@ import { useParams } from "react-router";
 import { useAppDispatch, useAppSelector } from "../hooks/hooks";
 import { getOneProduct, selectProducts } from "../redux/slices/productsSlice";
 
-interface ProductPageProps {}
+import "./ProductPage.scss";
 
-export const ProductPage: React.FC<ProductPageProps> = ({}) => {
+export const ProductPage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const dispatch = useAppDispatch();
   const { productDetails, loading, error } = useAppSelector(selectProducts);
-
-  useEffect(() => {
-    dispatch(getOneProduct({ id }));
-  }, [id]);
-
-  if (loading) return <h3>Loading...</h3>;
-
-  if (error) return <h3>{JSON.stringify(error, null, 2)}</h3>;
 
   const {
     name,
@@ -32,11 +24,33 @@ export const ProductPage: React.FC<ProductPageProps> = ({}) => {
     comments,
     countInStock,
   } = productDetails;
+
+  useEffect(() => {
+    dispatch(getOneProduct({ id }));
+  }, [id, dispatch]);
+
+  if (loading) return <h3>Loading...</h3>;
+
+  if (error) return <h3>{JSON.stringify(error, null, 2)}</h3>;
+
   return (
-    <div className="div">
-      <img src={imgURLs[0]} alt={name} />
-      <h1>{name}</h1>
-      <p>{longDescription}</p>
+    <div className="container">
+      <div className="row">
+        <section className="product__header">
+          <div className="product__images">
+            <div className="image__main">
+              <img src={imgURLs[0]} alt={name} />
+            </div>
+            <div className="image__others">
+              {imgURLs.map((image, index) => (
+                <img key={index} src={image} alt={name} />
+              ))}
+            </div>
+          </div>
+          <h1>{name}</h1>
+        </section>
+        <p>{longDescription}</p>
+      </div>
     </div>
   );
 };
