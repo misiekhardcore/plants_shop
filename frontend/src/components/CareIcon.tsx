9f57da9fd572713b0423b879ba54ddcc9945b963
misiekhardcore <misiekhardcore@gmail.com>
2021-05-21 14:49:18 +0200

    add care icons component

---
+++ frontend/src/components/CareIcon.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+
+const IconContainer = styled.div`
+  width: 80px;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img {
+    width: 100%;
+    max-height: 100%;
+  }
+`;
+
+interface CareIconProps {
+  level?: "low" | "medium" | "high";
+  temperature?: number;
+  type: "light" | "temp" | "water";
+}
+
+export const CareIcon: React.FC<CareIconProps> = ({
+  level = "medium",
+  temperature,
+  type = "light",
+}) => {
+  if (temperature) {
+    if (temperature < 16) level = "low";
+    if (temperature > 23) level = "high";
+    else level = "medium";
+  }
+  const img = `/assets/svg/${type}/${level}.svg`;
+  return (
+    <IconContainer>
+      <img src={img} alt={type} />
+    </IconContainer>
+  );
+};
