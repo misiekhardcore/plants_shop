bb79c68e0eb70a48fcb7b62fe5738fbee7a71709
misiekhardcore <misiekhardcore@gmail.com>
2021-05-23 17:53:02 +0200

    redesign

--- frontend/src/Pages/ProductPage.tsx
+++ frontend/src/Pages/ProductPage.tsx
@@ -6,16 +6,22 @@ import { ImageGallery } from "../components/ImageGallery";
 import { Rating } from "../components/Rating";
 import { useAppDispatch, useAppSelector } from "../hooks/hooks";
 import { usePageTitle } from "../hooks/usePageTitle";
-import { getOneProduct, selectProducts } from "../redux/slices/productsSlice";
+import { addToCart } from "../redux/slices/cartSlice";
+import {
+  getOneProduct,
+  selectProducts,
+} from "../redux/slices/productsSlice";
 
 import "./ProductPage.scss";
 
 export const ProductPage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const dispatch = useAppDispatch();
-  const { productDetails, loading, error } = useAppSelector(selectProducts);
+  const { productDetails, loading, error } =
+    useAppSelector(selectProducts);
   const [pictureNumber, setPictureNumber] = useState<number>(0);
   const [toggleGallery, setToggleGallery] = useState<boolean>(false);
+  const [inputAmount, setInputAmount] = useState<number>(1);
 
   const {
     name,
@@ -47,15 +53,26 @@ export const ProductPage: React.FC = () => {
     <div className="container">
       <div className="row">
         <section className="product__header">
-          <img
-            className="product__image img"
-            onClick={() => setToggleGallery(true)}
-            src={imgURLs[pictureNumber]}
-            alt={name}
-          />
+          <div className="product__image">
+            <img
+              className=" img large"
+              onClick={() => setToggleGallery(true)}
+              src={imgURLs[pictureNumber]}
+              alt={name}
+            />
+            {discount > 0 && (
+              <div className="product__discount">-{discount}%</div>
+            )}
+          </div>
           <article className="product__info">
             <h1 className="title">{name}</h1>
             <p className="shortDesc">{description}</p>
+            <img
+              className="product__image img small"
+              onClick={() => setToggleGallery(true)}
+              src={imgURLs[pictureNumber]}
+              alt={name}
+            />
             <div className="product__images">
               {imgURLs.map((image, index) => (
                 <img
@@ -72,9 +89,59 @@ export const ProductPage: React.FC = () => {
               <CareIcon type="water" level={watering} />
               <CareIcon type="temp" temperature={temperature} />
             </div>
-            <div style={{ display: "flex", justifyContent: "flex-end" }}>
-              <Rating rating={0} style={{ marginRight: "2rem" }} size="big" />
-              <Button size="big">add to cart</Button>
+            <div
+              style={{ display: "flex", justifyContent: "flex-end" }}
+            >
+              <Rating
+                rating={0}
+                style={{ marginRight: "2rem" }}
+                size="big"
+              />
+              <div style={{ display: "flex" }}>
+                <p>${price}</p>
+                <Button
+                  size="normal"
+                  onClick={() =>
+                    dispatch(
+                      addToCart({
+                        ...productDetails,
+                        amount: inputAmount,
+                      })
+                    )
+                  }
+                >
+                  add to cart
+                </Button>
+                <button
+                  onClick={() =>
+                    setInputAmount((p) => (p - 1 < 1 ? p : p - 1))
+                  }
+                >
+                  -
+                </button>
+                <input
+                  style={{
+                    marginLeft: "1rem",
+                    minWidth: "0px",
+                    width: "3rem",
+                  }}
+                  type="number"
+                  min={1}
+                  max={countInStock}
+                  step={1}
+                  value={inputAmount}
+                  onChange={(e) => setInputAmount(+e.target.value)}
+                />
+                <button
+                  onClick={() =>
+                    setInputAmount((p) =>
+                      p + 1 > countInStock ? p : p + 1
+                    )
+                  }
+                >
+                  +
+                </button>
+              </div>
             </div>
           </article>
         </section>
