35ece7dc74b774a1061cac4154d1716aecf61434
misiekhardcore <misiekhardcore@gmail.com>
2021-05-23 17:52:41 +0200

    add missing dependencie

--- frontend/src/hooks/usePageTitle.ts
+++ frontend/src/hooks/usePageTitle.ts
@@ -3,5 +3,5 @@ import { useEffect } from "react";
 export const usePageTitle = (title?: string) => {
   useEffect(() => {
     document.title = `Plant Shop${title ? ` - ${title}` : ''}`;
-  }, []);
+  }, [title]);
 };
