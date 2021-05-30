ba5472ab5cac1731c683d9e54d1e3641112c4df5
misiekhardcore <misiekhardcore@gmail.com>
2021-05-30 21:37:30 +0200

    redesign cart item, add functionality

--- frontend/src/components/CartProduct.tsx
+++ frontend/src/components/CartProduct.tsx
@@ -1,29 +1,98 @@
 import React from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import { Link } from "react-router-dom";
 import { useAppDispatch } from "../hooks/hooks";
-import { removeFromCart } from "../redux/slices/cartSlice";
+import { PlusMinusButton } from "../Pages/ProductPage";
+import { removeFromCart, updateCart } from "../redux/slices/cartSlice";
 import { ICartProduct } from "../types/types";
+import { Button } from "./Button";
+import styled from "styled-components";
 
-import "./CartProduct.scss";
+const Product = styled.li`
+  display: grid;
+  grid-template-columns: 100px 2fr 1fr 1fr auto;
+  justify-items: center;
+  align-items: center;
+  margin-bottom: 1rem;
+  // border: 1px solid red;
+  height: 100px;
+
+  img {
+    object-fit: cover;
+    display: block;
+    width: 100%;
+    max-height: 100%;
+  }
+`;
 
 interface CartProductProps {
   product: ICartProduct;
 }
 
-export const CartProduct: React.FC<CartProductProps> = ({ product }) => {
-  const { _id, name, amount, imgURLs, price } = product;
+export const CartProduct: React.FC<CartProductProps> = ({
+  product,
+}) => {
+  const { _id, name, amount, imgURLs, price, countInStock } = product;
   const dispatch = useAppDispatch();
   return (
-    <li className="cartproduct">
-      <div className="cartproduct__image">
-        <img
-          src={imgURLs[0] || "https://picsum.photos/id/111/300/200"}
-          alt={name}
-        />
-      </div>
-      <p>{name}</p>
+    <Product>
+      <img
+        src={imgURLs[0] || "https://picsum.photos/id/111/300/200"}
+        alt={name}
+      />
+      <Link to={`/products/${_id}`}>
+        <p>{name}</p>
+      </Link>
       <p>${price}</p>
-      <p>{amount}</p>
-      <button onClick={() => dispatch(removeFromCart({ id: _id }))}>X</button>
-    </li>
+      <div style={{ display: "flex" }}>
+        <PlusMinusButton
+          onClick={() => {
+            dispatch(
+              updateCart({
+                _id,
+                amount: amount > 1 ? amount - 1 : 1,
+              })
+            );
+          }}
+        >
+          <AiOutlineMinus />
+        </PlusMinusButton>
+        <input
+          style={{
+            minWidth: "0px",
+            width: "3rem",
+          }}
+          type="number"
+          min={1}
+          max={countInStock}
+          step={1}
+          value={amount}
+          onChange={(e) => {
+            dispatch(updateCart({ _id, amount: +e.target.value }));
+          }}
+        />
+        <PlusMinusButton
+          style={{
+            background: "none",
+            border: "none",
+            padding: "6px",
+          }}
+          onClick={() => {
+            dispatch(
+              updateCart({
+                _id,
+                amount:
+                  amount < countInStock ? amount + 1 : countInStock,
+              })
+            );
+          }}
+        >
+          <AiOutlinePlus />
+        </PlusMinusButton>
+      </div>
+      <Button onClick={() => dispatch(removeFromCart({ id: _id }))}>
+        X
+      </Button>
+    </Product>
   );
 };
