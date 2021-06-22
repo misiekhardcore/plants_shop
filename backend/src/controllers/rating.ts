238243e6bda721d05ce588fd77065b27d3072d1d
misiekhardcore <misiekhardcore@gmail.com>
2021-06-22 23:41:45 +0200

    create rating functionaity (need type fixing)

--- backend/src/controllers/rating.ts
+++ backend/src/controllers/rating.ts
@@ -1,16 +1,27 @@
-import { Response } from "express";
+import { NextFunction, Response } from "express";
 import { CustomReqBody, Error } from "../interfaces/common";
 import { IProduct } from "../interfaces/product";
 import { IRating } from "../interfaces/rating";
 import { Rating } from "../models/rating.model";
+import { getOneProduct } from "./products";
 
 export const createRating = async (
   req: CustomReqBody<IRating>,
-  res: Response<IProduct | Error>
+  res: Response<IProduct | Error>,
+  next: NextFunction
 ) => {
-  const ratings = await Rating.create({
-    user: req.userId,
-    product: req.body.product,
-  });
-  console.log(ratings);
+  try {
+    await Rating.create({
+      rating: req.body.rating,
+      user: req.userId,
+      product: req.body.product,
+    });
+    req.params.id = req.body.product;
+    const product = await getOneProduct(req, res, next);
+    if (!product)
+      return res.status(500).json({ message: "server error" });
+    return res.status(201).json(product);
+  } catch (error) {
+    return res.status(500).json({ message: "server error" });
+  }
 };
