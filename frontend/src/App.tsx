b9203c9c93dd6a4cac2daa06f63a8320d00d5e98
misiekhardcore <misiekhardcore@gmail.com>
2021-08-04 21:15:11 +0200

    Add 404 exception for api URL

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,6 +12,7 @@ import { CheckoutPage } from "./Pages/CheckoutPage";
 import CreateProductPage from "./Pages/CreateProductPage";
 import { LoginPage } from "./Pages/LoginPage";
 import MainPage from "./Pages/MainPage";
+import NoMatchPage from "./Pages/NoMatchPage";
 // import NoMatchPage from "./Pages/NoMatchPage";
 import { ProductPage } from "./Pages/ProductPage";
 import ProductsPage from "./Pages/ProductsPage";
@@ -20,30 +21,36 @@ import { RegisterPage } from "./Pages/RegisterPage";
 import { lightTheme } from "./theme";
 
 const App: React.FC = () => {
-  return (
-    <ThemeProvider theme={lightTheme}>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={MainPage} />
-          <Route exact path="/login" component={LoginPage} />
-          <Route exact path="/register" component={RegisterPage} />
-          <Route exact path="/cart" component={CartPage} />
-          <Route exact path="/checkout" component={CheckoutPage} />
-          <Route exact path="/products" component={ProductsPage} />
-          <Route exact path="/products/:id" component={ProductPage} />
-          <Route exact path="/profile" component={ProfilePage} />
-          <Route
-            exact
-            path="/admin/create-product"
-            component={CreateProductPage}
-          />
-          {/* <Route path="*" component={NoMatchPage} /> */}
-        </Switch>
-        <Footer />
-      </Router>
-    </ThemeProvider>
-  );
+  const api_regex = /^\/api\/.*/;
+  // if using "/api/" in the pathname, don't use React Router
+  if (api_regex.test(window.location.pathname)) {
+    return <div />; // must return at least an empty div
+  } else {
+    return (
+      <ThemeProvider theme={lightTheme}>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={MainPage} />
+            <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/register" component={RegisterPage} />
+            <Route exact path="/cart" component={CartPage} />
+            <Route exact path="/checkout" component={CheckoutPage} />
+            <Route exact path="/products" component={ProductsPage} />
+            <Route exact path="/products/:id" component={ProductPage} />
+            <Route exact path="/profile" component={ProfilePage} />
+            <Route
+              exact
+              path="/admin/create-product"
+              component={CreateProductPage}
+            />
+            <Route path="*" component={NoMatchPage} />
+          </Switch>
+          <Footer />
+        </Router>
+      </ThemeProvider>
+    );
+  }
 };
 
 export default App;
